9d0d479e2203c239cd7f5c86616322014780f7a9
Christoffer G. Thomsen <chris@cgt.name>
2020-11-12 17:51:24 +0100

    Simplify example test
    The whole Hiker thing is just too much. I never want it.

---
+++ start_point/FizzBuzzTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FizzBuzzTest {
+    @Test
+    void example() {
+        assertEquals(true, true);
+    }
+}
